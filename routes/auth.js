fd6cd7c03fccd0acbe18e378e5c0a67d7a20f31e
Kunal Arneja <kunal.arneja@narvar.com>
2020-09-08 13:29:53 +0530

    initial commit bigcommerce app

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+router = express.Router(),
+BigCommerce = require('node-bigcommerce');
+
+const bigCommerce = new BigCommerce({
+clientId: 'd9nnt8i14fn9werfbrmygr4ttqe0brk',
+secret: '60ca6397790bbf08a9ddf701253975c4204c6db79d8daf2e70baf10dae542668',
+callback: 'https://ac98b2aeb41b.ngrok.io/auth',
+responseType: 'json'
+});
+
+router.get('/', (req, res, next) => {
+bigCommerce.authorize(req.query)
+.then(data => console.log(data))
+.then(data => res.render('auth', { title: 'Authorized!' })
+.catch(err));
+});
+module.exports = router;
---
+++ routes/uninstall.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+router = express.Router(),
+BigCommerce = require('node-bigcommerce');
+const bigCommerce = new BigCommerce({
+secret: '60ca6397790bbf08a9ddf701253975c4204c6db79d8daf2e70baf10dae542668',
+responseType: 'json'
+});
+
+router.get('/', (req, next) => {
+try {
+const data = bigCommerce.verify(req.query['signed_payload']);
+console.log(data);
+} catch (err) {
+next(err);
+}
+});
+
+module.exports = router;
